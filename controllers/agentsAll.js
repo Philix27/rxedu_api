b63462581f829001d6af203e6af4db20b73d0806
Philix27 <philixbob@gmail.com>
2022-11-29 12:20:51 +0100

    update agentsall

--- controllers/agentsAll.js
+++ controllers/agentsAll.js
@@ -1,6 +1,5 @@
 const Agents = require("../models/agent");
 const asyncWrapper = require("../middleware/asyncWrapper");
-const { createCustomError } = require("../errors/custom-error");
 
 const getAll = asyncWrapper(async (req, res, next) => {
   const data = await Agents.find();
